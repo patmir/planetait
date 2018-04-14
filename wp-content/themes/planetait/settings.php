82648d3c5d04871e3cacb40495a2812209b8427e
Patryk Mirosław <miroslaw.patryk@gmail.com>
2018-04-15 00:00:15 +0200

    3

--- wp-content/themes/planetait/settings.php
+++ wp-content/themes/planetait/settings.php
@@ -4,7 +4,10 @@
  * Prevent Direct Access
  */
 defined( 'ABSPATH' ) or die( "Restricted access!" );
-
+wp_enqueue_script( 'nav-menu' );
+require_once( ABSPATH . 'wp-admin/admin-header.php' );
+$settings_url = admin_url("admin.php?page=".GlobalConfig::get("project_slug_name")."_ustawienia");
+$action =  $_GET['action'];
 /**
  * Render Settings Tab Content
  */
@@ -14,9 +17,9 @@ global $wp;
  <h1 class="wp-heading-inline"><?= GlobalConfig::get("project_pretty_name")?> - Ustawienia</h1>
  <hr class="wp-header-end">
  <h2 class="nav-tab-wrapper wp-clearfix">
- <a href="<?= home_url($wp->request)?>" class="nav-tab nav-tab">Ustawienia Ogólne</a>
- <a href="<?= home_url($wp->request)?>?action=tresc" class="nav-tab">Ustawienia Treści</a>
- <a href="<?= home_url($wp->request)?>?action=slider" class="nav-tab">Slider</a>
+ <a href="<?= $settings_url ?>" class="nav-tab <?php if ( ! isset( $_GET['action'] ) ) echo ' nav-tab-active'; ?>">Ustawienia Ogólne</a>
+ <a href="<?= $settings_url ?>&action=tresc" class="nav-tab <?php if (isset($_GET['action']) && 'tresc' == $_GET['action']) echo "nav-tab-active";?>"s>Ustawienia Treści</a>
+ <a href="<?= $settings_url ?>&action=slider" class="nav-tab <?php if (isset($_GET['action']) && 'slider' == $_GET['action']) echo "nav-tab-active";?>">Slider</a>
  </h2>
 <button id="ustawienia_generuj_tresci">Generuj Treści</button>
 </div>
