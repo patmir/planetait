5c2da65a973828e7e6fdc4faa95ac3661f21c33f
Patryk Mirosław <miroslaw.patryk@gmail.com>
2018-04-24 00:36:06 +0200

    qwe

--- wp-content/themes/planetait/header.php
+++ wp-content/themes/planetait/header.php
@@ -20,6 +20,8 @@ $logo = wp_get_attachment_image_src($logo, "full")[0];
 		<link href="http://fonts.googleapis.com/css?family=Poppins:700%2C400%2C500" rel="stylesheet" property="stylesheet" type="text/css" media="all">
 		<link href="https://fonts.googleapis.com/css?family=Ubuntu:300,300i,400,400i,500,500i,700,700i&amp;subset=latin-ext" rel="stylesheet" property="stylesheet" type="text/css" media="all">
 		<link href="https://fonts.googleapis.com/css?family=Gloria+Hallelujah" rel="stylesheet" media="all">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="description" content="<?php bloginfo('description'); ?>">
---
+++ wp-content/themes/planetait/templates/contact-template.php
@@ -0,0 +1,162 @@
+<?php
+/*
+Template Name: Contact
+Template Post Type: subpage
+*/
+get_header();
+?>
+<div id="rev_slider_53_1_wrapper" class="rev_slider_wrapper fullwidthbanner-container" data-alias="contact" data-source="gallery"
+	 style="margin:0px auto;background:#ffffff;padding:0px;margin-top:0px;margin-bottom:0px;">
+		<!-- START REVOLUTION SLIDER 5.4.7 fullwidth mode -->
+		<div id="rev_slider_53_1" class="rev_slider fullwidthabanner" style="display:none;" data-version="5.4.7">
+			<ul>
+				<!-- SLIDE  -->
+				<li data-index="rs-111" data-transition="fade" data-slotamount="default" data-hideafterloop="0" data-hideslideonmobile="off"
+				 data-easein="default" data-easeout="default" data-masterspeed="300" data-rotate="0" data-saveperformance="off" data-title="Slide"
+				 data-param1="" data-param2="" data-param3="" data-param4="" data-param5="" data-param6="" data-param7="" data-param8=""
+				 data-param9="" data-param10="" data-description="">
+					<!-- MAIN IMAGE -->
+					<img src="<?= get_template_directory_uri() ?>/assets/images/transparent.png" alt="" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" class="rev-slidebg"
+					 data-no-retina>
+					<!-- LAYERS -->
+
+					<!-- LAYER NR. 1 -->
+					<div class="tp-caption tp-shape tp-shapewrapper  tp-resizeme" id="slide-111-layer-20" data-x="center" data-hoffset="" data-y="330"
+					 data-width="['960']" data-height="['640']" data-type="shape" data-responsive_offset="on" data-frames='[{"delay":"bytrigger","speed":1500,"frame":"0","from":"y:[-100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;","mask":"x:0px;y:0px;s:inherit;e:inherit;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"bytrigger","speed":300,"frame":"999","to":"opacity:0;","ease":"Power3.easeInOut"}]'
+					 data-textAlign="['inherit','inherit','inherit','inherit']" data-paddingtop="[0,0,0,0]" data-paddingright="[0,0,0,0]"
+					 data-paddingbottom="[0,0,0,0]" data-paddingleft="[0,0,0,0]" data-lasttriggerstate="keep" style="z-index: 5;background-color:rgb(255,255,255);-webkit-box-shadow:inset 0px 0px 10px 0px rgba(0,0,0,0.75);-moz-box-shadow:inset 0px 0px 10px 0px rgba(0,0,0,0.75);box-shadow:inset 0px 0px 10px 0px rgba(0,0,0,0.75);">
+					</div>
+
+					<!-- LAYER NR. 2 -->
+					<div class="tp-caption tp-shape tp-shapewrapper  tp-resizeme" id="slide-111-layer-11" data-x="center" data-hoffset="" data-y="10"
+					 data-width="['960']" data-height="['320']" data-type="shape" data-responsive_offset="on" data-frames='[{"delay":10,"speed":300,"frame":"0","from":"opacity:0;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;","ease":"Power3.easeInOut"}]'
+					 data-textAlign="['inherit','inherit','inherit','inherit']" data-paddingtop="[0,0,0,0]" data-paddingright="[0,0,0,0]"
+					 data-paddingbottom="[0,0,0,0]" data-paddingleft="[0,0,0,0]" style="z-index: 6;background-color:rgb(255,255,255);-webkit-box-shadow:0px 0px 10px 0px rgba(0,0,0,0.75);-moz-box-shadow:0px 0px 10px 0px rgba(0,0,0,0.75);box-shadow:0px 0px 10px 0px rgba(0,0,0,0.75);">
+					</div>
+
+					<!-- LAYER NR. 3 -->
+					<div class="tp-caption   tp-resizeme" id="slide-111-layer-12" data-x="center" data-hoffset="-240" data-y="10" data-width="['none','none','none','none']"
+					 data-height="['none','none','none','none']" data-type="image" data-responsive_offset="on" data-frames='[{"delay":600,"speed":300,"frame":"0","from":"z:0;rX:0;rY:0;rZ:0;sX:0.9;sY:0.9;skX:0;skY:0;opacity:0;","to":"o:1;","ease":"Power2.easeOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;","ease":"Power3.easeInOut"}]'
+					 data-textAlign="['inherit','inherit','inherit','inherit']" data-paddingtop="[0,0,0,0]" data-paddingright="[0,0,0,0]"
+					 data-paddingbottom="[0,0,0,0]" data-paddingleft="[0,0,0,0]" style="z-index: 7;">
+						<img src="<?= get_template_directory_uri() ?>/img/cities/radom.jpg" alt="" data-ww="480px" data-hh="320px" data-no-retina> </div>
+
+					<!-- LAYER NR. 4 -->
+					<div class="tp-caption   tp-resizeme" id="slide-111-layer-13" data-x="500" data-y="32" data-width="['auto']" data-height="['auto']"
+					 data-type="text" data-responsive_offset="on" data-frames='[{"delay":900,"speed":300,"frame":"0","from":"y:50px;opacity:0;fb:10px;","to":"o:1;fb:0;","ease":"Power4.easeOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;fb:0;","ease":"Power3.easeInOut"}]'
+					 data-textAlign="['inherit','inherit','inherit','inherit']" data-paddingtop="[0,0,0,0]" data-paddingright="[0,0,0,0]"
+					 data-paddingbottom="[0,0,0,0]" data-paddingleft="[0,0,0,0]" style="z-index: 8; white-space: nowrap; font-size: 36px; line-height: 48px; font-weight: 400; color: #4d4b4b; letter-spacing: 0px;font-family:Ubuntu;">RADOM </div>
+
+					<!-- LAYER NR. 5 -->
+					<div class="tp-caption tp-shape tp-shapewrapper  tp-resizeme" id="slide-111-layer-14" data-x="720" data-y="29" data-width="['3']"
+					 data-height="['55']" data-type="shape" data-responsive_offset="on" data-frames='[{"delay":1500,"speed":300,"frame":"0","from":"y:[-100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;","mask":"x:0px;y:0px;s:inherit;e:inherit;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;","ease":"Power3.easeInOut"}]'
+					 data-textAlign="['inherit','inherit','inherit','inherit']" data-paddingtop="[0,0,0,0]" data-paddingright="[0,0,0,0]"
+					 data-paddingbottom="[0,0,0,0]" data-paddingleft="[0,0,0,0]" style="z-index: 9;background-color:rgb(193,20,55);"> </div>
+
+					<!-- LAYER NR. 6 -->
+					<div class="tp-caption   tp-resizeme" id="slide-111-layer-15" data-x="760" data-y="26" data-width="['auto']" data-height="['auto']"
+					 data-type="text" data-responsive_offset="on" data-frames='[{"delay":1200,"speed":300,"frame":"0","from":"y:50px;opacity:0;fb:10px;","to":"o:1;fb:0;","ease":"Power4.easeOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;fb:0;","ease":"Power3.easeInOut"}]'
+					 data-textAlign="['inherit','inherit','inherit','inherit']" data-paddingtop="[0,0,0,0]" data-paddingright="[0,0,0,0]"
+					 data-paddingbottom="[0,0,0,0]" data-paddingleft="[0,0,0,0]" style="z-index: 10; white-space: nowrap; font-size: 14px; line-height: 20px; font-weight: 400; color: #4d4b4b; letter-spacing: 0px;font-family:Open Sans;">PLANETA IT Radom
+						<br> Kalińska 3
+						<br> 26-600 Radom </div>
+
+					<!-- LAYER NR. 7 -->
+					<div class="tp-caption   tp-resizeme" id="slide-111-layer-16" data-x="500" data-y="106" data-width="['auto']" data-height="['auto']"
+					 data-type="text" data-responsive_offset="on" data-frames='[{"delay":2100,"split":"lines","splitdelay":0.2,"speed":2000,"split_direction":"forward","frame":"0","from":"y:50px;opacity:0;fb:10px;","to":"o:1;fb:0;","ease":"Power4.easeOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;fb:0;","ease":"Power3.easeInOut"}]'
+					 data-textAlign="['inherit','inherit','inherit','inherit']" data-paddingtop="[0,0,0,0]" data-paddingright="[0,0,0,0]"
+					 data-paddingbottom="[0,0,0,0]" data-paddingleft="[0,0,0,0]" style="z-index: 11; white-space: nowrap; font-size: 14px; line-height: 20px; font-weight: 500; color: #0a0a0a; letter-spacing: 0px;font-family:Ubuntu;">Biuro
+						<br> tel.: +48 796 520 220
+						<br> info@planetait.eu </div>
+
+					<!-- LAYER NR. 8 -->
+					<div class="tp-caption   tp-resizeme" id="slide-111-layer-17" data-x="500" data-y="182" data-width="['auto']" data-height="['auto']"
+					 data-type="text" data-responsive_offset="on" data-frames='[{"delay":2400,"split":"lines","splitdelay":0.2,"speed":2000,"split_direction":"forward","frame":"0","from":"y:50px;opacity:0;fb:10px;","to":"o:1;fb:0;","ease":"Power4.easeOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;fb:0;","ease":"Power3.easeInOut"}]'
+					 data-textAlign="['inherit','inherit','inherit','inherit']" data-paddingtop="[0,0,0,0]" data-paddingright="[0,0,0,0]"
+					 data-paddingbottom="[0,0,0,0]" data-paddingleft="[0,0,0,0]" style="z-index: 12; white-space: nowrap; font-size: 14px; line-height: 20px; font-weight: 500; color: #0a0a0a; letter-spacing: 0px;font-family:Ubuntu;">Wsparcie techniczne:
+						<br> Księgowość
+						<br> HR:
+						<br> Business Development </div>
+
+					<!-- LAYER NR. 9 -->
+					<div class="tp-caption   tp-resizeme" id="slide-111-layer-18" data-x="760" data-y="182" data-width="['auto']" data-height="['auto']"
+					 data-type="text" data-responsive_offset="on" data-frames='[{"delay":3000,"split":"lines","splitdelay":0.2,"speed":2000,"split_direction":"forward","frame":"0","from":"y:50px;opacity:0;fb:10px;","to":"o:1;fb:0;","ease":"Power4.easeOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;fb:0;","ease":"Power3.easeInOut"}]'
+					 data-textAlign="['inherit','inherit','inherit','inherit']" data-paddingtop="[0,0,0,0]" data-paddingright="[0,0,0,0]"
+					 data-paddingbottom="[0,0,0,0]" data-paddingleft="[0,0,0,0]" style="z-index: 13; white-space: nowrap; font-size: 14px; line-height: 20px; font-weight: 500; color: #0a0a0a; letter-spacing: 0px;font-family:Ubuntu;">support@planetait.eu
+						<br> ksiegowosc@planetait.eu
+						<br> hr@planetait.eu
+						<br> business@planetait.eu </div>
+
+					<!-- LAYER NR. 10 -->
+					<div class="tp-caption rev-btn " id="slide-111-layer-19" data-x="639" data-y="282" data-width="['auto']" data-height="['auto']"
+					 data-type="button" data-actions='[{"event":"click","action":"startlayer","layer":"slide-111-layer-20","delay":"100"},{"event":"click","action":"startlayer","layer":"slide-111-layer-21","delay":"1000"}]'
+					 data-responsive_offset="on" data-responsive="off" data-frames='[{"delay":3400,"speed":2000,"frame":"0","from":"y:50px;opacity:0;fb:10px;","to":"o:1;fb:0;","ease":"Power4.easeOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;fb:0;","ease":"Power3.easeInOut"},{"frame":"hover","speed":"0","ease":"Linear.easeNone","to":"o:1;rX:0;rY:0;rZ:0;z:0;fb:0;","style":"c:rgb(255,255,255);bg:linear-gradient(180deg, rgba(193,20,55,1) 0%, rgba(146,28,121,1) 100%);bs:solid;bw:0 0 0 0;"}]'
+					 data-textAlign="['inherit','inherit','inherit','inherit']" data-paddingtop="[10,10,10,10]" data-paddingright="[30,30,30,30]"
+					 data-paddingbottom="[10,10,10,10]" data-paddingleft="[30,30,30,30]" style="z-index: 14; white-space: nowrap; font-size: 14px; line-height: 14px; font-weight: 500; color: rgba(255,255,255,1); letter-spacing: px;font-family:Ubuntu;background:linear-gradient(180deg, rgba(193,20,55,1) 0%, rgba(146,28,121,1) 100%);border-color:rgba(0,0,0,1);border-radius:3px 3px 3px 3px;outline:none;box-shadow:none;box-sizing:border-box;-moz-box-sizing:border-box;-webkit-box-sizing:border-box;cursor:pointer;">NAPISZ DO NAS </div>
+
+					<!-- LAYER NR. 11 -->
+					<div class="tp-caption  " id="slide-111-layer-21" data-x="0" data-y="352" data-width="['auto']" data-height="['auto']" data-type="text"
+					 data-responsive_offset="on" data-responsive="off" data-frames='[{"delay":"bytrigger","speed":600,"frame":"0","from":"y:50px;opacity:0;fb:10px;","to":"o:1;fb:0;","ease":"Power4.easeOut"},{"delay":"bytrigger","speed":300,"frame":"999","to":"opacity:0;fb:0;","ease":"Power3.easeInOut"}]'
+					 data-textAlign="['inherit','inherit','inherit','inherit']" data-paddingtop="[0,0,0,0]" data-paddingright="[0,0,0,0]"
+					 data-paddingbottom="[0,0,0,0]" data-paddingleft="[0,0,0,0]" data-lasttriggerstate="keep" style="z-index: 15; min-width: 600px; max-width: 600px; white-space: nowrap; font-size: 20px; line-height: 22px; font-weight: 400; color: #4d4b4b; letter-spacing: 0px;font-family:Ubuntu;"><div class="container-fluid" style="max-width: 960px;"><div class="row justify-content-center"><div class="col-sm"><?= do_shortcode('[contact-form-7 id="226" title="Form-PL"]')?></div></div></div></div>
+				</li>
+			</ul>
+			<div class="tp-bannertimer tp-bottom" style="visibility: hidden !important;"></div>
+		</div>
+	</div>
+	<!-- END REVOLUTION SLIDER -->
+    <script type="text/javascript">
+    var templateUrl = "<?= get_template_directory_uri() ?>";
+		var revapi53,
+			tpj;
+		(function () {
+			if (!/loaded|interactive|complete/.test(document.readyState)) document.addEventListener("DOMContentLoaded", onLoad)
+			else
+				onLoad();
+
+			function onLoad() {
+				if (tpj === undefined) {
+					tpj = jQuery;
+
+					if ("off" == "on") tpj.noConflict();
+				}
+				if (tpj("#rev_slider_53_1").revolution == undefined) {
+					revslider_showDoubleJqueryError("#rev_slider_53_1");
+				} else {
+					revapi53 = tpj("#rev_slider_53_1").show().revolution({
+						sliderType: "standard",
+						jsFileLocation: templateUrl+"/js/lib/revolution/js/",
+						sliderLayout: "fullwidth",
+						dottedOverlay: "none",
+						delay: 9000,
+						navigation: {
+							onHoverStop: "off",
+						},
+						visibilityLevels: [1240, 1024, 778, 480],
+						gridwidth: 960,
+						gridheight: 868,
+						lazyType: "none",
+						shadow: 0,
+						spinner: "spinner0",
+						stopLoop: "off",
+						stopAfterLoops: -1,
+						stopAtSlide: -1,
+						shuffle: "off",
+						autoHeight: "off",
+						disableProgressBar: "on",
+						hideThumbsOnMobile: "off",
+						hideSliderAtLimit: 0,
+						hideCaptionAtLimit: 0,
+						hideAllCaptionAtLilmit: 0,
+						debugMode: false,
+						fallbacks: {
+							simplifyAll: "off",
+							nextSlideOnWindowFocus: "off",
+							disableFocusListener: false,
+						}
+					});
+				}; /* END OF revapi call */
+			}; /* END OF ON LOAD FUNCTION */
+		}()); /* END OF WRAPPING FUNCTION */
+	</script>
+<?php get_footer();?>
