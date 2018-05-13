ea94f717f78959896d85ab80c9b9e95869dd6c1a
Patryk Mirosław <miroslaw.patryk@gmail.com>
2018-05-13 12:35:28 +0200

    favicon

--- wp-content/themes/planetait/header.php
+++ wp-content/themes/planetait/header.php
@@ -17,6 +17,23 @@ $logo = wp_get_attachment_image_src($logo, "full")[0];
 		   <link rel="preload" href="<?php echo get_template_directory_uri(); ?>/css/lib/fonts/Gloria_Hallelujah/gloria.woff2" as="font" type="font/woff2" crossorigin="anonymous">
 		   <link rel="preload" href="<?php echo get_template_directory_uri(); ?>/css/lib/fonts/Ubuntu/Ubuntu-Regular.ttf" as="font" type="font/woff2" crossorigin="anonymous">
 		   <link rel="preload" href="<?php echo get_template_directory_uri(); ?>/css/lib/fonts/Ubuntu/Ubuntu-Bold.ttf" as="font" type="font/woff2" crossorigin="anonymous">
+		   <!-- FAV ICONS -->
+		   <link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_template_directory_uri(); ?>/icon/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_template_directory_uri(); ?>/icon/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/icon/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_directory_uri(); ?>/icon/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/icon/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_template_directory_uri(); ?>/icon/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/icon/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_template_directory_uri(); ?>/icon/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/icon/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="<?php echo get_template_directory_uri(); ?>/icon/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/icon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="<?php echo get_template_directory_uri(); ?>/icon/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/icon/favicon-16x16.png">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/icon/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
 
 <!-- External Styling -->
 		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/lib/normalize.css">
